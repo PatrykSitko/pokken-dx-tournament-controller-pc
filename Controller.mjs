25957f082a79eb7c89184215de90a6388055d559
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-18 07:40:11 +0100

    Modified transfer and added methods

--- Controller.mjs
+++ Controller.mjs
@@ -1,5 +1,6 @@
 import { MissingEndpointError } from './Controller.errors.mjs';
 import requireJSON from './modules/requireJSON.mjs';
+import { errorCodes } from './Controller.errors.mjs';
 import './modules/json/cycle.mjs';
 
 const mappingSchemas = requireJSON('./json/controller-mappings.json');
@@ -11,6 +12,7 @@ export default class Controller {
   buttons = [];
   previousButtons = undefined;
   notify = [];
+  startedMonitoring = false;
   constructor(controller, buttonMapping) {
     this.controller = controller;
     this.buttonMapping = buttonMapping;
@@ -24,7 +26,21 @@ export default class Controller {
   get deviceAddress() {
     return this.controller.deviceAddress;
   }
+
+  hasStartedMonitoring() {
+    return this.startedMonitoring;
+  }
+  isMonitoring() {
+    for (let isMonitoring of Object.values(this.running)) {
+      if (!isMonitoring) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   startMonitoring() {
+    this.startedMonitoring = true;
     if (this.controller) {
       this.controller.open();
       this.interface = this.controller.interface(0);
@@ -100,20 +116,23 @@ const loop = {
     this.running.endpointListener = true;
     return setTimeout(
       async function() {
-        let command = await transfer(this.endpoint);
-        command = command.substring(0, 6);
-        command = command.endsWith('f')
-          ? `${command.substring(0, command.length - 1)}0`
-          : command.endsWith('0')
-          ? `${command.substring(0, command.length - 1)}f`
-          : command;
-        this.command = command;
-        mapButtons.bind(this)();
+        let command = await transfer.bind(this)();
+        if (command) {
+          command = command.substring(0, 6);
+          command = command.endsWith('f')
+            ? `${command.substring(0, command.length - 1)}0`
+            : command.endsWith('0')
+            ? `${command.substring(0, command.length - 1)}f`
+            : command;
+          this.command = command;
+          mapButtons.bind(this)();
+        }
         clearTimeout(this.timeout.endpointListener);
         if (this.running.endpointListener) {
           this.timeout.endpointListener = loop.endpointListener.bind(this)();
         }
-      }.bind(this)
+      }.bind(this),
+      1
     );
   },
   inputEmitter: function() {
@@ -136,16 +155,29 @@ const loop = {
       if (this.running.inputEmitter) {
         this.timeout.inputEmitter = loop.inputEmitter.bind(this)();
       }
-    });
+    }, 1);
   }
 };
 
-function transfer(endpoint) {
-  return new Promise((resolve, reject) =>
-    endpoint.transfer(endpoint.descriptor.wMaxPacketSize, (err, data) => {
-      err ? reject(err) : resolve(data.toString('hex'));
-    })
-  );
+async function transfer() {
+  try {
+    return await new Promise((resolve, reject) =>
+      this.endpoint.transfer(
+        this.endpoint.descriptor.wMaxPacketSize,
+        (err, data) => {
+          err ? reject(err) : resolve(data.toString('hex'));
+        }
+      )
+    );
+  } catch (error) {
+    console.log(error, error.errno);
+    switch (error.errno) {
+      case errorCodes.LIBUSB_TRANSFER_ERROR:
+      case errorCodes.LIBUSB_ERROR_NOT_FOUND:
+        this.stopMonitoring();
+        break;
+    }
+  }
 }
 function formatButtonMapping(buttonMapping) {
   const regroupedButtonMapping = {};
