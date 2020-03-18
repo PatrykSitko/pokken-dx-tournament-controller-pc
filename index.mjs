a0cf6fede983022e2a6b6f511c1e36fe48b72013
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2020-03-18 21:00:58 +0100

    Fixed connection issue

--- index.mjs
+++ index.mjs
@@ -2,25 +2,27 @@ import Controllers from './Controllers.mjs';
 import robot from 'robotjs';
 
 let previousButtons = [];
-const controllers = new Controllers().getControllers();
-for (let controller of controllers) {
-  controller.startMonitoring();
-  controller.addInputListener(({ schema, buttons }) => {
-    if (JSON.stringify(buttons) !== JSON.stringify(previousButtons)) {
-      if (buttons.length > 0) {
-        for (let button of buttons) {
-          if (previousButtons.includes(button)) {
-            continue;
+const controllers = new Controllers();
+setInterval(() => {
+  for (let controller of controllers.get()) {
+    controller.startMonitoring();
+    controller.addInputListener(({ schema, buttons }) => {
+      if (JSON.stringify(buttons) !== JSON.stringify(previousButtons)) {
+        if (buttons.length > 0) {
+          for (let button of buttons) {
+            if (previousButtons.includes(button)) {
+              continue;
+            }
+            robot.keyToggle(schema[button], 'down');
+            previousButtons.push(button);
           }
-          robot.keyToggle(schema[button], 'down');
-          previousButtons.push(button);
+        } else {
+          for (const button of previousButtons) {
+            robot.keyToggle(schema[button], 'up');
+          }
+          previousButtons = [];
         }
-      } else {
-        for (const button of previousButtons) {
-          robot.keyToggle(schema[button], 'up');
-        }
-        previousButtons = [];
       }
-    }
-  });
-}
+    });
+  }
+}, 1);
